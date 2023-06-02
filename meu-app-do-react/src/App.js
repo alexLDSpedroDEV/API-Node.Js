bd2c6195c8e286c1708758ab316aa92794e85770
AlexLopesdaSilvaPedro <115034319+alexLDSpedroDEV@users.noreply.github.com>
2023-06-02 12:28:42 -0300

    atualização

--- meu-app-do-react/src/App.js
+++ meu-app-do-react/src/App.js
@@ -2,16 +2,13 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import ListCarros from './componentes/listCarros';
 
-const allCar = () => {
-    
-  return <ListCarros />;
-}
+
 
 function App() {
 
   return (
     <div>
-      <button onClick={allCar}>enviar</button>
+      <ListCarros />
       
     </div>
   );
--- meu-app-do-react/src/componentes/listCarros.js
+++ meu-app-do-react/src/componentes/listCarros.js
@@ -3,31 +3,56 @@ import axios from 'axios'
 import style from './listCarro.module.css'
 export default class ListCarros extends React.Component{
     
-    state={
+   
+    arrey={
         carros:[]
     }
-    
+
+
     componentDidMount(){
         axios.get('http://localhost:8080/person/')
         .then(res => {
             const dadosCarros=res.data;
             this.setState({carros: dadosCarros})
+            
         })
+        
     }
+    
+
+    //Criando um json para armazenar os dados do backend
     constructor(props){
         super(props)
+        
         //estou criando json para enviar ao backend
         this.state = {
             name: "",
             valor: "",
             marca: "",
         }
+
+        //enviando tudos que esta no constructor para o handleSubmit
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    //função para criar novos carros no banco de dados
+    handleSubmit(e) {
+
+        //pevinindo recarregamento da pagina
+        e.preventDefault();
+        
+        
+        //enviando os dados do state (json que criamos) para a url do backend
+        axios.post('http://localhost:8080/person/', this.state)
+        .then(res => {
+            alert("Registrado com sucesso")
+        })
     }
     
     render() {
         return(
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
 
                     <h3>Registrar Novo Carro</h3>
 
@@ -37,6 +62,7 @@ export default class ListCarros extends React.Component{
                         placeholder='Digite o nome do carro'
                         type='text'
                         className='form_input'
+                        onChange={ e => this.setState({name: e.target.value})}
                         />
                     </div>
 
@@ -46,6 +72,7 @@ export default class ListCarros extends React.Component{
                         placeholder='Digite a marca do carro'
                         type='text'
                         className='form_input'
+                        onChange={ e => this.setState({marca: e.target.value})}
                         />
                     </div>
 
@@ -55,19 +82,22 @@ export default class ListCarros extends React.Component{
                         placeholder='Digite o valor do carro'
                         type='text'
                         className='form_input'
+                        onChange={ e => this.setState({valor: e.target.value})}
                         />
                     </div>
 
-
+                    <button type='submit'>enviar</button>
                 </form>
-                {/* {this.state.carros.map(carro=>
+
+
+                {this.arrey.carros.map(carro=>
                     <div id='dd' className={style.div} key={carro._id}>
                         <p>{carro.name}</p>
                         <p>{carro.valor}</p>
                         <p>{carro.marca}</p>
                         <img src={carro.url}/>
                     </div>
-                )} */}
+                )}
             </div>
         )
     }
