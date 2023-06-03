a7178b3e852dbcc95b31a44771738538a3ef0cca
AlexLopesdaSilvaPedro <115034319+alexLDSpedroDEV@users.noreply.github.com>
2023-06-03 09:10:17 -0300

    atulização

--- meu-app-do-react/src/App.js
+++ meu-app-do-react/src/App.js
@@ -1,29 +1,46 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import ListCarros from './componentes/listCarros';
-import CreatedCar from './componentes/createdCar';
-
-
-
-
-
-const setActive = (props) => {
-  if(props == true) {
-    return <CreatedCar />
-  } else {
-    <ListCarros />
-  }
-}
+import CreatedCar from './componentes/createdCar'
 
 function App() {
 
+  const [tela, setTela] = useState(0)
+
+  useEffect(() => {
+    const url = window.location.href
+    const res = url.split('?')
+    setTela(res[1])
+  })
+
+  const linkPaginas = (e) => {
+    if(e == 1) {
+      window.open('http://localhost:3000?1','_self')
+    } else if (e == 2) {
+      window.open('http://localhost:3000?2','_self')
+    }
+  }
+
+  console.log(tela)
+
+  const retunrpage = () => {
+    if(tela == 1){
+      return <ListCarros/>
+    } else if (tela == 2) {
+      return <CreatedCar/>
+    } else {
+      return <div>
+        <button onClick={() => linkPaginas(1)}>paina 1</button>
+        <button onClick={() => linkPaginas(2)}>paina 2</button>
+      </div>
+    }
+  }
+
+
   return (
     <div>
-      <h1> Estacionamento de Carros</h1>
-
-      <button onClick={setActive}> registrar novo carro</button>
       
-      
+      {retunrpage()}
      
       
     </div>
